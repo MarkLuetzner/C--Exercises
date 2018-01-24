ebed61a4abeee3323129758ac4b7b13658bcc619
vonluetzen <mark.luetzner@gmx.de>
2018-01-24 18:27:11 +0100

    p 446

---
+++ Exercise_p446/Exercise_p446/CardComparer_bySuit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_p446
+{
+    class CardComparer_bySuit : IComparer<Card>
+    {
+        public int Compare(Card x, Card y)
+        {
+            if (x.Suit < y.Suit)
+                return -1;
+            if (x.Suit > y.Suit)
+                return 1;
+            else
+                return 0;
+        }
+    }
+}
---
+++ Exercise_p446/Exercise_p446/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace Exercise_p446
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Deck deckToWrite = RandomDeck(5);
+            using (Stream output = File.Create("Deck1.dat"))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(output, deckToWrite);
+            }
+            DealCards(deckToWrite, "What I just wroite to the file");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (Stream input = File.OpenRead("Deck1.dat"))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                Deck deckFromFile = (Deck)bf.Deserialize(input);
+                DealCards(deckFromFile, "What I read from the file");
+            }
+        }
+
+        private Deck RandomDeck(int number)
+        {
+            Random random = new Random();
+            Deck myDeck = new Deck(new Card[] { });
+            for (int i = 0; i < number; i++)
+            {
+                myDeck.Add(new Card((Suits)random.Next(4), (Values)random.Next(1, 14)));
+            }
+            return myDeck;
+        }
+
+        private void DealCards(Deck deckToDeal, string title)
+        {
+            Console.WriteLine(title);
+            while (deckToDeal.Count > 0)
+            {
+                Card nextCard = deckToDeal.Deal(0);
+                Console.WriteLine(nextCard.Name);
+            }
+            Console.WriteLine("----------");
+        }
+
+    }
+}
